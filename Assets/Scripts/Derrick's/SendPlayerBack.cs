feea1b7daae2e9eff5e3203e00726fc933195aa0
vaporiiise <derick6266@gmail.com>
2024-11-14 03:05:02 +0800

    u33
    -added new "Chase" level for navigating halls
    -added 3d walls for Inner halls

--- Assets/Scripts/Derrick's/SendPlayerBack.cs
+++ Assets/Scripts/Derrick's/SendPlayerBack.cs
@@ -1,42 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class SendPlayerBack : MonoBehaviour
 {
-    public GameObject player;       // Assign the player GameObject in the inspector
-    public float targetZPosition;   // The target Z position
-    public Vector3 requiredPosition;  // The position where the key press will work
-    public float positionTolerance = 0.1f;  // How close the player needs to be to the required position
+    public GameObject player;       
+    public float targetZPosition;   
+    public Vector3 requiredPosition;  
+    public float positionTolerance = 0.1f;
+    public GameObject objectToDisable;
+    public GameObject objectToDisable2;
+    public Vector3 moveObject;
+    public Light2D globalLight;
+    public float intensity;
+    public GameObject playerLight;
 
     void Update()
     {
-        // Check if the player is within a certain range of the required position
         if (Vector3.Distance(player.transform.position, requiredPosition) <= positionTolerance)
         {
-            // Check if the "F" key is pressed
             if (Input.GetKeyDown(KeyCode.F))
             {
-                // Move the player to the target Z position
                 Vector3 currentPosition = player.transform.position;
                 player.transform.position = new Vector3(currentPosition.x, currentPosition.y, targetZPosition);
+                objectToDisable.transform.position = Vector3.MoveTowards(objectToDisable.transform.position, moveObject, 10 * Time.deltaTime);
+                globalLight.intensity = intensity;
+                objectToDisable2.SetActive(false);
+                playerLight.SetActive(true);
             }
         }
     }
 
-    // Draw gizmos in the Scene view to visualize the required position and tolerance area
     void OnDrawGizmos()
     {
-        // Set the gizmo color to green
         Gizmos.color = Color.green;
-        
-        // Draw a sphere at the required position
         Gizmos.DrawSphere(requiredPosition, 0.1f);
-        
-        // Set the gizmo color to red (for the tolerance area)
         Gizmos.color = Color.red;
-        
-        // Draw a wireframe sphere around the required position with the given tolerance
         Gizmos.DrawWireSphere(requiredPosition, positionTolerance);
     }
 }
