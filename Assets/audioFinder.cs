d096b4202e312dc087c8147a0b15e8b022a043b7
Mayonnaise <156632738+Jasonliew99@users.noreply.github.com>
2025-03-18 23:33:30 +0800

    u80
    Bosshall(Pause&Volume controls done)
    
    issue:
    -tick sound for some reason doesnt register as sfx

---
+++ Assets/audioFinder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioFinder : MonoBehaviour
+{
+    public AudioClip targetClip; // Assign the SFX you are looking for in Inspector
+
+    void Update()
+    {
+        AudioSource[] allAudioSources = FindObjectsOfType<AudioSource>();
+
+        foreach (AudioSource audio in allAudioSources)
+        {
+            if (audio.isPlaying && audio.clip == targetClip)
+            {
+                Debug.Log("🔊 Playing: " + targetClip.name + " on " + audio.gameObject.name, audio.gameObject);
+            }
+        }
+    }
+}
